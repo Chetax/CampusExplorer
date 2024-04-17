e4dc0e0e3ca79a3e4c89c83b9bb4badbfd8fe6be
Chetan Navnath Padhen <padhenchetan@gmail.com>
2024-04-17 21:58:56 +0530

    m

--- Backend/Routes/faculty.js
+++
@@ -1,7 +0,0 @@
-const express =require('express');
-const router=express.Router();
-const createFaculty =require('../Controller/CreateFaculty');
-const getFaculty=require('../Controller/getFaculty');
-router.get('/getFacultyInfo',getFaculty);
-router.post("/createFaculty",createFaculty);
-module.exports=router;
